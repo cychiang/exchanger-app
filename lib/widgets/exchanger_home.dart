c69335780a045a10ed01dcd64025c1a4c958e7aa
cychiang <cychiang0823@gmail.com>
2018-07-17 23:37:26 +0800

    add state monitor

--- lib/widgets/exchanger_home.dart
+++ lib/widgets/exchanger_home.dart
@@ -8,12 +8,39 @@ class ExchangerHome extends StatefulWidget {
   _ExchangerHomeState createState() => new _ExchangerHomeState();
 }
 
-class _ExchangerHomeState extends State<ExchangerHome> {
+class _ExchangerHomeState extends State<ExchangerHome>
+    with WidgetsBindingObserver {
+  AppLifecycleState _lastLifecycleState;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      _lastLifecycleState = state;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final exchangerBloc = ExchangerProvider.of(context);
     // TODO: Add base currency
     // TODO: Add functionality to select target currency
+    if (_lastLifecycleState == null) {
+      print(_lastLifecycleState);
+    } else {
+      print(_lastLifecycleState);
+    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Exchanger'),
